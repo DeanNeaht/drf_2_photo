4ac184f360860a9e115e57966ce95d13eb958651
Nikolay Solodovnikov <snugp123@gmail.com>
2021-12-13 22:17:27 +0300

    sfbgdnb

--- drf_2_photo/urls.py
+++ drf_2_photo/urls.py
@@ -13,9 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from save_photo.views import ImageUploadView
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="STRT",
+      default_version='v1',
+      description="Test description",
+      contact=openapi.Contact(name='tg @gqblsd'),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui(cache_timeout=0), name='schema-json'),
+    path('uploud/', ImageUploadView.as_view()),
 ]
